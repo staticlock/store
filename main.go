4f137588f7e6481e5e7b9624b8bfd1407b357583
staticlock <2133525474@qq.com>
2024-10-31 13:33:40 +0800

    first commit

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import "fmt"
-<<<<<<< HEAD
-
-func main() {
-	fmt.Println("hello")
-=======
-import "store/testpackage"
+import (
+	"fmt"
+	"store/testpackage"
+)
 
 func main() {
 	fmt.Println("hello")
 	testpackage.Myfunc()
->>>>>>> 29be44d (first commit)
 }
